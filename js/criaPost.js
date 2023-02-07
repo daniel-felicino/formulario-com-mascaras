20fa1fb37b17e5f406d7333b82cb67678f7b9f41
https://github.com/daniel-felicino <daniel-feliciano@hotmail.com>
2023-02-07 16:42:22 -0300

    atualização

--- js/criaPost.js
+++ js/criaPost.js
@@ -21,7 +21,6 @@ async function requisicaoPost(id, cpf, cartao, cnpj, celular, email) {
 
     //a const resultadoPost vai receber a variavel requisição e vai converter em um json
     const resultadoPost = await requisicao.json()
-    console.log(resultadoPost + "aqui")
 }
 // vou ter 2 formas de exportar 1 primeira é colcoar o 
 // export direto no asunc e esundo assim : export const requisicaoPostEnviar = requisicaoPost
